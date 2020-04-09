500c964fb4effbe21257838fbc40164b437b72bc
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2020-04-09 20:53:02 +0100

    rename and ignore start up

---
+++ WebSQL/__Start_up.cs
@@ -0,0 +1,31 @@
+﻿//using Entity.Models;
+//using Microsoft.Owin;
+//using Owin;
+//using SDHC.Common.Entity.Models;
+//using Start;
+//using System;
+//using System.Collections.Generic;
+//using System.Web.Hosting;
+//using System.Web.Mvc;
+//using System.Web.Routing;
+//using WebSQL.Models;
+
+//[assembly: OwinStartupAttribute(typeof(WebSQL.Startup))]
+//namespace WebSQL
+//{
+//  public partial class Startup
+//  {
+//    public void Configuration(IAppBuilder app)
+//    {
+//      SDHCStartup.Init<ApplicationDbContext, SCHCContent, SDHCBascSelect, MyUser>(
+//        app, () => ApplicationDbContext.Create(), HostingEnvironment.MapPath("/"));
+//      ServiceContainer.ModelService.ModelMapper = new Dictionary<string, Type>()
+//      {
+//        ["S1"] = typeof(S1),
+//        ["S2"] = typeof(S2),
+//        ["Gender"] = typeof(GenderSelect),
+//      };
+//    }
+//  }
+  
+//}
